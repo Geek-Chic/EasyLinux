fb9283861277f22b9f67a7119f603f57edf02796
evil <evilester@sina.com>
2014-12-31 10:58:52 +0800

    修正根目录设定，使easyc全局可以调用

--- Module/Macos/command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# File Name:command.zz
-# Description:MAC common command
-# @Author: evil
-# Created Time:Tue 30 Dec 2014 02:09:26 PM CST
-
-GETOPT="/usr/local/Cellar/gnu-getopt/1.1.5/bin/getopt"
